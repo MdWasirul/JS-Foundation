9a54778703137e0b306edfbe67319082a48de4e0
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-19 15:13:39 +0530

    let var const scope

---
+++ part1/change.js
@@ -0,0 +1,30 @@
+let gameName="Spiderman";
+gameName="Batsman";
+console.log(gameName);  //->batman
+
+const userName="Rahul choudhary";
+userName="hitesh";
+console.log(userName);
+/*
+What is Good?
+let and const have block scope.
+
+let and const can not be redeclared.
+
+let and const must be declared before use.
+
+let and const does not bind to this.
+
+let and const are not hoisted.
+
+*. Variables declared with the var always have Global Scope.
+
+
+What is Not Good?
+var does not have to be declared.
+
+var is hoisted.
+
+var binds to this.
+
+*/
