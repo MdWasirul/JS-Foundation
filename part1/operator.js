66fd47809c7114b6d96572aeca246e80cb14b87a
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-19 15:31:15 +0530

    operator

--- part1/operator.js
+++ part1/operator.js
@@ -1,7 +1,21 @@
-let score=102;
-let bonus=25;
+let score = 102;
+let bonus = 25;
 
-let resultScore=score +bonus;
-console.log(resultScore);
+let resultScore = score + bonus;
+console.log(resultScore); //->127;
 
-let addition=4+5;
+let addition = 4 + 5;
+console.log(addition);
+let subtract = 8 - 3;
+let multiple = 4 * 7;
+let division = 18 / 3;
+
+let remainder = 18 % 3;
+let expon=2 ** 3;
+
+let myscore=110;
+myscore++;
+// ++myscore;
+
+let credits=65;
+credits--;
