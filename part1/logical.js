a05fc5004c40890c29d93b45823a2c9bbe7aaf16
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-19 16:01:57 +0530

    logical

---
+++ part1/logical.js
@@ -0,0 +1,13 @@
+// && -->logical and
+// || -->logical or
+// ! --> logical not (reverse)
+
+let isLoggedIn=true ;
+// let isPaid=false;
+let isPaid=true;
+
+// console.log(isLoggedIn && isPaid);
+
+let isEmailuser=true;
+let isgoogleuser=false;
+console.log(isEmailuser || isgoogleuser);
