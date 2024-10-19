fa06e8989389a349431d40c5478ae04f0a6fcd88
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-19 14:58:35 +0530

    data types

---
+++ part1/datatype.js
@@ -0,0 +1,25 @@
+console.log("Hello");
+// console.log("chai");
+/* This multiple comment
+----datatypes---
+String 
+Number
+BigInt
+null 
+undefined
+Boolean
+symbol
+
+Objects
+*/
+// var score=102;
+let score=102;
+let name="ChaiWalla";
+let isLoggedin=false;
+
+//object
+let teatypes=["lemon ","orange","oolong"];
+let usr={firstname:"Rahul",lastname:"kumar"}
+
+let getScore=score;
+console.log(getScore);
