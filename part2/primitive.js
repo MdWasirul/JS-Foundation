caf7e3ec7b4726e41de4aea0c131d99bae238c9f
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-20 12:15:12 +0530

    primitve

--- part2/primitive.js
+++ part2/primitive.js
@@ -1,4 +1,12 @@
 //Number
 
 let balance =120;
-console.log(typeof(balance));
+
+let anotherbalance=new Number(125);  //goal to give types of 
+
+// console.log(balance);
+// console.log(anotherbalance);
+// console.log(anotherbalance.valueOf());   //-> 125.
+
+console.log(typeof(balance));          // Number.
+console.log(typeof(anotherbalance));   // Objeccts.
