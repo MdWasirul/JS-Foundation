ed25e9e748219f55a25fe4942791f9b27fef7695
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-20 12:42:48 +0530

    primitive /part2

--- part2/primitive.js
+++ part2/primitive.js
@@ -8,5 +8,21 @@ let anotherbalance=new Number(125);  //goal to give types of
 // console.log(anotherbalance);
 // console.log(anotherbalance.valueOf());   //-> 125.
 
-console.log(typeof(balance));          // Number.
-console.log(typeof(anotherbalance));   // Objeccts.
+// console.log(typeof(balance));          // Number.
+// console.log(typeof(anotherbalance));   // Objeccts.
+
+//Boolean -data types
+let isActive =true;
+let isReallyActive =new Boolean(true);  //This is not a recommending way.
+console.log(typeof(isActive));   //type -> boolean.
+console.log(typeof(isReallyActive));   //type->Object
+
+//Null and undefined
+/*
+Key Differences:
+null is typically used when you want to explicitly set a variable or object to be empty or have no value.
+undefined is the default state of a variable that has been declared but not assigned a value.
+
+Both null and undefined are loosely equal (==), but they are not strictly equal (===):
+
+*/
