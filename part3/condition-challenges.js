598a20425ee418eb70da366b27c67f6e42677b44
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-20 20:28:20 +0530

    conditions challenges

--- part3/condition-challenges.js
+++ part3/condition-challenges.js
@@ -1,9 +1,34 @@
-//Condition if a number is greater than another number.
+// 1.Condition if a number is greater than another number.
 let num1=5;
 let num2=8;
-    
+
+/*
+    console.log("I am regular upper code");
 if(num1 >num2){
         console.log("num1 is greater than num2");
 }else{
     console.log("num1 is less than num2.");
+}
+*/
+
+// 2.Check if a String is equal to another string:
+
+// let username="Kunal";
+// let anotherUsername="Kunal";
+// if(username!=anotherUsername){
+//     console.log("Pick another username");
+// }else{
+//     console.log("You can pick a name");
+// }
+
+
+//3.Check if a variable is a number or not.....
+// in case of passing score value string -->No ,that is not a number
+//// in case of passing score value array like  []  -->No ,that is not a number
+let score=45;
+
+if(typeof score==='number'){
+    console.log("Yep ,this is a number");
+} else{
+    console.log("No ,that is not a number");
 }
