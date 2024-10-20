dafc1a095a35f3472d503ca692f31db8972fdd4d
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-20 20:03:31 +0530

    condition /part3

---
+++ part3/condition-challenges.js
@@ -0,0 +1,9 @@
+//Condition if a number is greater than another number.
+let num1=5;
+let num2=8;
+    
+if(num1 >num2){
+        console.log("num1 is greater than num2");
+}else{
+    console.log("num1 is less than num2.");
+}
