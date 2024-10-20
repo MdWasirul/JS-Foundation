cf08db267ee88269dde9e5f613c3737e528de358
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-20 20:43:32 +0530

    conditionals

--- part3/condition-challenges.js
+++ part3/condition-challenges.js
@@ -23,12 +23,45 @@ if(num1 >num2){
 
 
 //3.Check if a variable is a number or not.....
+
 // in case of passing score value string -->No ,that is not a number
 //// in case of passing score value array like  []  -->No ,that is not a number
-let score=45;
+// let score=45;
 
-if(typeof score==='number'){
-    console.log("Yep ,this is a number");
-} else{
-    console.log("No ,that is not a number");
+// if(typeof score==='number'){
+//     console.log("Yep ,this is a number");
+// } else{
+//     console.log("No ,that is not a number");
+// }
+
+//4. check if a boolean value is true or false;
+
+// let isReady=false;
+// if(isReady){
+//     console.log("Tea is  ready");
+// }else{
+//     console.log("Tea is not ready");
+// }
+
+//checking if an arary is Empty or Not ...
+
+// let items=[];
+
+// console.log(items.length);
+// if(items.length===0){
+//     console.log("Array is Empty");  /*output->   0
+//                                                Array is Empty */
+// }else{
+//     console.log("Array is not Empty");
+// }
+
+//---------------if arrays has item---------------------
+let items=["kunal"];
+
+console.log(items.length);
+if(items.length===0){
+    console.log("Array is Empty");  /*output->   0
+                                               Array is Empty */
+}else{
+    console.log("Array is not Empty");
 }
