431fa8faca200baaba594039eb3ef2e599bd18f8
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-10-26 20:09:11 +0530

    levelUp Challenges

--- part4/levelUpChalenge.js
+++ part4/levelUpChalenge.js
@@ -1,4 +1,4 @@
-/*Wrte `for` loop that loop through array `["green taea","black tea","chai","oolong tea"]`
+/*  1.Wrte `for` loop that loop through array `["green taea","black tea","chai","oolong tea"]`
 and stops the loop when it finds "chai"
 stores all the teas before `"chai"` in a new array named  `selectedTea`.
 */
@@ -13,16 +13,46 @@ if(teas[i]==="chai"){
 }
 // console.log(selectedTea);     //[ 'green taea', 'black tea' ]
 
-/*Wrte `for` loop that loop through array `["Londan","New York","Paris","Berlin"]`
+/* 2.Wrte `for` loop that loop through array `["Londan","New York","Paris","Berlin"]`
 and skips the `"Paris"`
 Store the new cities in a new array named `visitedCities`.
 */
 let cities=["Londan","New York","Paris","Berlin"];
 let visitedCities=[];
 for(let i=0; i<cities.length;i++){
-    if(cities[i]==="Paris"){
+    if(cities[i]==="Paris" || cities[i]==="paris"){
         continue;
     }
     visitedCities.push(cities[i]);
 }
-console.log(visitedCities);
+// console.log(visitedCities);
+
+/* 3.use `for-of` loop to iterate through the array `[1,2,3,4,5];`.
+and stops when the number 4 is found .
+Stores the number before `4` in an array named `SmallNUmbers`.
+*/
+let numbers=[1,2,3,4,5];
+let SmallNUmbers=[];
+for (const num of numbers) {
+    if(num===4){
+        break;
+    }
+  SmallNUmbers.push(num);
+}
+// console.log("num: ",SmallNUmbers);   //num:  [ 1, 2, 3 ]
+
+
+
+/* 5.use` for-in ` loop to loop  through an object containing city populations .
+Stop the loop when the population of  `Berlin` is found and Store all previous cities `
+`populations` in  A  NEW OBJECT named `cityPopulations`.
+*/
+let citiesPopulation={
+    "Londan":8900000,
+    "New Yark":84000000,
+    "Paris":22000000,
+    "Berlin":3500000,
+};
+let cityPopulation={};
+console.log(Object.keys(citiesPopulation));  //[ 'Londan', 'New Yark', 'Paris', 'Berlin' ]
+
