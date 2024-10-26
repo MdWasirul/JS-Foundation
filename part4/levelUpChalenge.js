8ffb4b6e0399399af969bfab45348af41ba31262
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-10-26 19:08:59 +0530

    loop 10Challenges

---
+++ part4/levelUpChalenge.js
@@ -0,0 +1,5 @@
+/*Wrte `for` loop that loop through array `["green taea","black tea","chai","oolong tea"]`
+and stops thr loop when it finds "chai"
+stores all the teas before `"chai"` in a new array named  `selectedTea`.
+*/
+
