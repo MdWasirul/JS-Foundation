5fa8e8d09ba06548b0434a9e4efb163528af9764
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-11-07 23:43:03 +0530

    Prototypes

--- part6/prototypes.js
+++ part6/prototypes.js
@@ -1,3 +1,19 @@
+/*
+All JavaScript objects inherit properties and methods from a prototype.
+
+Prototype Inheritance
+All JavaScript objects inherit properties and methods from a prototype:
+
+Date objects inherit from Date.prototype
+Array objects inherit from Array.prototype
+Person objects inherit from Person.prototype
+The Object.prototype is on the top of the prototype inheritance chain:
+
+Date objects, Array objects, and Person objects inherit from Object.prototype.
+
+
+*/
+
 let computer={cpu:12,ram:4};
 let lenovo={
     screen:"HD",
@@ -18,3 +34,6 @@ Object.setPrototypeOf(tesla,genericCar);
 console.log(`tesla`, tesla); //tesla { driver: 'AI' }
 
 console.log(`tesla`, genericCar); //tesla { tyres: 4 }
+console.log(`tesla`, tesla.tyres); //tesla 4
+
+console.log(`tesla`,Object.getPrototypeOf(tesla));  //tesla { tyres: 4 }
