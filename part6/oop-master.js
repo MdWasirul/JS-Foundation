be1d784e2ec6c0db8b062f21422c026b0eff0b7a
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-11-13 19:36:16 +0530

    Polymorphism

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -115,3 +115,42 @@ let myMachine=new CofeeMachine();
 // console.log(myMachine.start());
 // console.log(myMachine.brewCofee());
 console.log(myMachine.pressStartButton()); //Starting the Machine .......... + BrewCoffee
+ 
+//Polymorphism
+class Bird{
+  fly(){
+    return `Bird flying......`
+  }
+}
+class Penguin extends Bird{
+  fly(){
+    return `Penguin can't fly..`
+  }
+}
+let bird=new Bird();
+let penguin=new Penguin();
+console.log(bird.fly());    //Bird flying......
+console.log(penguin.fly()); //Penguin can't fly..
+
+
+//Static method 
+class Calculator{
+  static add(a,b){
+    return a+b;
+  }
+}
+// let miniCalculator=new Calculator();
+// console.log(miniCalculator.add(3,5)); //mini Calculator.add is not a function
+
+console.log(Calculator.add(5,6)); //11
+
+//getters and setters
+
+class Employee{
+  constructor(name,salary){
+    this.name=name;
+    this._salary=salary;
+  }
+}
+let emp=new Employee("Alice",50000)
+console.log(emp._salary);
