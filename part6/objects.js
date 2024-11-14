3b6aee3ef20902269a23b7b169f651e512ecc935
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-11-15 00:13:56 +0530

    objects Chai or code

--- part6/objects.js
+++ part6/objects.js
@@ -6,3 +6,18 @@ console.log(multiply5(5));
 console.log(multiply5.power);
 console.log(multiply5.prototype);
 
+function CreateUser(username,score){
+    this.username=username;
+    this.score=score;
+
+}
+CreateUser.prototype.increment=function(){
+    this.score++;
+
+}
+CreateUser.prototype.printMe=function(){
+    console.log(`price is ${this.score}`);
+}
+const chai=new CreateUser("chai",45);
+const tea=new CreateUser("tea",340);
+chai.printMe();
