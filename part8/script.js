653f4e8d02e3c88b399f42d480562aca5c84649c
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-11-19 17:59:36 +0530

    dom /bom example2

--- part8/script.js
+++ part8/script.js
@@ -4,10 +4,24 @@
 document
   .getElementById("changeTextButton")
   .addEventListener("click", function () {
-    console.log(this); //return button element
+    // console.log(this); //return button element
+   const para=document.getElementById("myParagraph");
+   console.log(para);
+//    console.log(para.textContent);
+  para.textContent="My Paragraph is changed"; 
   });
 //   document
 //   .getElementById("changeTextButton")
 //   .addEventListener("click",  ()=> {
 //     console.log(this); //return Window global object
 //   });
+
+//Example -2
+
+// const btn=document.getElementById("highlightFirstCity");
+// console.log(btn);
+document.getElementById("highlightFirstCity")
+.addEventListener("click",function(){
+  let citiesList=   document.getElementById("citiesList");
+  console.log(citiesList.firstElementChild.classList);
+}) ;
