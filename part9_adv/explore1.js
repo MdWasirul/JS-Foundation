1f1a5fca8f8a64842aa5c59efdb688afed4c61a4
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-09 22:07:04 +0530

    Asynchronous in  JS

---
+++ part9_adv/explore1.js
@@ -0,0 +1,15 @@
+console.log("chai code ");
+
+function sayHello(){
+    console.log("I would like to take tea!");
+}
+setTimeout(() => {
+    sayHello();
+   
+}, 3000);
+
+
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+    
+}
