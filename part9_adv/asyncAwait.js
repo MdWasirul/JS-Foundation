72275a0753730c7ce5a7809ea21fd6beffaed487
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-10 18:39:34 +0530

    async await

--- part9_adv/asyncAwait.js
+++ part9_adv/asyncAwait.js
@@ -1,20 +1,20 @@
 function fetchUserData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve({name:"chaiCode",url:"https://chaicode.com"});
-        console.log("Youtube is loaded ")
+      resolve({ name: "chaiCode", url: "https://chaicode.com" });
+      console.log("Youtube is loaded");
     }, 3000);
   });
 }
 async function getUserData() {
-    try {
-        console.log("Fetching User Data......")
-      const userData= await fetchUserData();
-      console.log("User data fetched successfuly");
-      console.log(userData);
-    } catch (error) {
-        console.log("Error fetching data",error);
-    }
+  try {
+    console.log("Fetching User Data......");
+    const userData = await fetchUserData();
+    console.log("User data fetched successfuly");
+    console.log(userData);
+  } catch (error) {
+    console.log("Error fetching data", error);
+  }
 }
 getUserData();
 
