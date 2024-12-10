fb63618ea442040299cbf8dd798562193d23ba17
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-10 20:36:16 +0530

    generator in JS

--- part9_adv/generator.js
+++ part9_adv/generator.js
@@ -6,11 +6,14 @@ yield 3
 
 }
 let gen=numGenerator();
+let genTwo=numGenerator();
 // console.log(gen);
 console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
 
+console.log(genTwo.next().value);
+console.log(genTwo.next().value);
 /*
 
 1
