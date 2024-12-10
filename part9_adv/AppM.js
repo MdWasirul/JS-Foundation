c8b9fe81caaabcbee46f8c00a43c406f7926ec10
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-10 23:00:52 +0530

    Module Js

---
+++ part9_adv/AppM.js
@@ -0,0 +1,13 @@
+
+//Module Import File
+
+//default import
+import multiply from "./MathOperationM.js";
+
+//named Import
+import {add,subtract} from "./MathOperationM.js";
+
+
+console.log(multiply(2,2));
+console.log(add(2,4));
+console.log(subtract(8,4));
