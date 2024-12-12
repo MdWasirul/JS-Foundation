e10a509a4b55aa89459893da0b155f029c8be922
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-12 15:44:26 +0530

    LocalStorage setting data into  and getting data from

--- part10_Project/Project1/AddTask.js
+++ part10_Project/Project1/AddTask.js
@@ -2,15 +2,32 @@ const todoInput = document.getElementById("todo-input");
 const AddTaskBtn = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
 
-const Task = [];
+const tasks = [];
 AddTaskBtn.addEventListener("click", () => {
-  const inputTask = todoInput.value.trim();
-  if (inputTask === "") return;
-  Task.push(inputTask);
-  console.log(Task);
-  todoInput.value = "";
+  const taskText = todoInput.value.trim();
+  if (taskText === "") return;
+  // Task.push(taskText);
+  // console.log(Task);
+  // todoInput.value = "";
+  //------create Object for task have id name etc
+  const newTask={
+    id:Date.now(),
+    text:taskText,
+    completed:false
+  }
+  tasks.push(newTask);
+  saveTask();
+  todoInput.value="";  //clear input
+  console.log(tasks);
+  
 });
 
+function renderTask(task){
+  console.log(task);
+}
+function saveTask(){
+  localStorage.setItem("tasks",JSON.stringify(tasks));
+}
 
 /*
 ----------in console-----------
