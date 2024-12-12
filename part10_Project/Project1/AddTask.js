eb3a4b12ae2a74eeb723f16b2bb7c8aaf3f04f0f
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-12 18:08:13 +0530

    AddTask Project1

--- part10_Project/Project1/AddTask.js
+++ part10_Project/Project1/AddTask.js
@@ -1,35 +1,66 @@
-const todoInput = document.getElementById("todo-input");
-const AddTaskBtn = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const AddTaskBtn = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-const tasks = [];
-AddTaskBtn.addEventListener("click", () => {
-  const taskText = todoInput.value.trim();
-  if (taskText === "") return;
-  // Task.push(taskText);
-  // console.log(Task);
-  // todoInput.value = "";
-  //------create Object for task have id name etc
-  const newTask={
-    id:Date.now(),
-    text:taskText,
-    completed:false
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => renderTask(task));
+
+  AddTaskBtn.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+    // Task.push(taskText);
+    // console.log(Task);
+    // todoInput.value = "";
+    //------create Object for task have id name etc
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTask();
+    renderTask(newTask);
+    todoInput.value = ""; //clear input
+    console.log(tasks);
+  });
+
+  function renderTask(task) {
+    // console.log(task);
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>`;
+
+    li.addEventListener("click", (e) => {
+      // console.log(e)
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTask();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTask();
+    });
+
+    todoList.appendChild(li);
   }
-  tasks.push(newTask);
-  saveTask();
-  todoInput.value="";  //clear input
-  console.log(tasks);
-  
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  /*
+  ----------in console-----------
+  (3) ['dfgdfs', 'dfgsdh', 'hdfhhdf']
+  */
+
+  //   todoInput.addEventListener("keypress", (e) => {
+  //   if (e.key === "Enter")
+  //     { addTaskBtn.click();}
+  // });
 });
-
-function renderTask(task){
-  console.log(task);
-}
-function saveTask(){
-  localStorage.setItem("tasks",JSON.stringify(tasks));
-}
-
-/*
-----------in console-----------
-(3) ['dfgdfs', 'dfgsdh', 'hdfhhdf']
-*/
