2d379c3ddf1052e28764209ba44fff657d741c1d
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-13 12:57:18 +0530

    TODO List Completed

--- part10_Project/Project1/addTasks.js
+++ part10_Project/Project1/addTasks.js
@@ -1,22 +1,22 @@
-//Pratice Session 
-const inputTask=document.getElementById("todo-input");
-const taskButton=document.getElementById("add-task-btn");
-const todoList=document.getElementById("todo-list");
+//Pratice Session
+const inputTask = document.getElementById("todo-input");
+const taskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
 
- const tasks= JSON.parse(localStorage.getItem("todoTasks"))||[];
-tasks.forEach((task )=> renderTask(task));
+let tasks = JSON.parse(localStorage.getItem("todoTasks")) || [];
+tasks.forEach((task) => renderTask(task));
 
+//add task on button click
 
-taskButton.addEventListener("click",()=>{
-   const taskText =inputTask.value.trim();
-   if(taskText==="")return;
+taskButton.addEventListener("click", () => {
+  const taskText = inputTask.value.trim();
+  if (taskText === "") return;
 
-   
-    //    tasks.push(taskText);
-    //    inputTask.value="";
-     //    console.log(tasks);
+  //    tasks.push(taskText);
+  //    inputTask.value="";
+  //    console.log(tasks);
 
-   const newTask = {
+  const newTask = {
     id: Date.now(),
     text: taskText,
     completed: false,
@@ -26,18 +26,47 @@ taskButton.addEventListener("click",()=>{
   saveTask();
   renderTask(newTask);
 
-  inputTask.value="";
+  inputTask.value = "";
   console.log(tasks);
-})
+});
 
+// Add Task on Enter Key Press
+inputTask.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    taskButton.click();
+  }
+});
 
-function renderTask(task){
-console.log(task);
+function renderTask(task) {
+  console.log(task);
+  const li = document.createElement("li");
+  li.setAttribute("data-id", task.id);
+  li.innerHTML = `
+      <span>${task.text} </span>
+      <button>delete</button>`;
 
+  //add Toggle
+  li.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") return;
+    task.completed = !task.completed;
+    li.classList.toggle("completed");
+    saveTask();
+  });
 
+  // Delete list item
+  li.querySelector("button").addEventListener("click", (e) => {
+    e.stopPropagation();
+    tasks = tasks.filter((t) => t.id !== task.id); //reassign the array
+    li.remove();       //removing li from DOM
+    saveTask();        //update the local Storage
+  });
+
+  todoList.appendChild(li);
 
 }
 
-function saveTask(){
-    localStorage.setItem("todoTasks",JSON.stringify(tasks));
+//LocalStorage setting
+
+function saveTask() {
+  localStorage.setItem("todoTasks", JSON.stringify(tasks));
 }
