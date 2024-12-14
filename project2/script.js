601b3596aa14dec78eaafb4f09fa192e4d52155a
The SwiftCoder <techbymdwasirul2024@gmail.com>
2024-12-14 12:03:52 +0530

    Project 2 Layout

---
+++ project2/script.js
@@ -0,0 +1,4 @@
+const cityInput = document.getElementById("city-input");
+const weatherButton=document.getElementById("get-weather-btn");
+const weatherInfo=document.getElementById("weather-info");
+const errorMessage=document.getElementById("error-message");
