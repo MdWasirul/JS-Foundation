6916ce424cbcd4416e5895586be249b831ec565d
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-01-02 15:59:57 +0530

    reacts jsx

--- React/01basicsvite/src/App.jsx
+++ React/01basicsvite/src/App.jsx
@@ -1,11 +1,11 @@
 import Youtube from "./Youtube"
 
 function App() {
-
+const username="hitesh"
   return (
     <>
-    <h1>Vite react app</h1>
-    <h1>Vite react app</h1>
+    <h1>Vite react app {2+3}</h1>
+    <h1>Vite react app {username}</h1>
     <Youtube/>
     </>
   )
--- React/01basicsvite/src/main.jsx
+++ React/01basicsvite/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
-)
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "click me to visit Google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>This is custom react App</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    visit Google
+  </a>
+);
+const anotherUser="chai or code"
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click to visit google",
+  anotherUser
+);
+createRoot(document.getElementById("root")).render(
+  //  AnotherElement
+  // reactElement
+  areactElement
+  // <MyApp/>
+  // <App/>
+);
