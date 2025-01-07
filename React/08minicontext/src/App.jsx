d893ec14c83a433eecb06f5da313e889e800c81d
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-01-07 20:17:09 +0530

    Context pi part 1

--- React/08minicontext/src/App.jsx
+++ React/08minicontext/src/App.jsx
@@ -1,11 +1,15 @@
-
 import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/profile";
+
 import UserContextProvider from "./context/UserContextprovider";
 
 function App() {
   return (
     <UserContextProvider>
       <h1>React with Hitesh and Share its important </h1>
+      <Login />
+      <Profile/>
     </UserContextProvider>
   );
 }
